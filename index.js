9db6b95b5390bf247f924b2008716ec7c1868dea
Charles  Ohene <ckumi7@gmail.com>
2018-08-07 04:18:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string){
+  console.log('hello')
+}
+
+function logWhisper(string){
+  console.log('HELLO')
+}
+
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string){
+  if(string.toLowerCase() === string)
   return "I can't hear you!";
-}if(string.toUpperCase() === string){
+}
+
+function sayHiToGrandma(string) {
+  if(string.toUpperCase() === string)
   return "YES INDEED!";
-  }if (string === "I love you, Grandma"){
+}
+
+function sayHiToGrandma(string){
+  if (string === "I love you, Grandma.")
   return "I love you, too.";
-  }}
+}
